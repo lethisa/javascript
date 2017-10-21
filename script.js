43721323c88083440bc14ffa73d42c611b49d7e3
lethisa <lethisaputri@gmail.com>
2017-10-21 15:55:28 +0700

    update script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Lecture: variables
 /*
 var name = 'Jhon';
@@ -13,8 +14,9 @@ var fullAge = true;
 console.log(fullAge);
 */
 
-/*
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Lecture : variables 2
+/*
 var name = 'Jhon';
 var age  = 26;
 
@@ -44,13 +46,16 @@ console.log(lastName);
 alert(name + ' is a ' + age + ' years old ' + job + ' .Is he married ' + isMarried + '.');
 */
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Lecture: operators
+/*
 var now = 2016;
 var birthYear = now - 26;
 // 2016 - 26 = 1990
 
 birthYear = now - 26 * 2;
 // 2016 - 52 = 1964
+// precedence
 
 console.log(birthYear);
 
@@ -69,3 +74,64 @@ ageMark = ageMark * 2;
 // similar operation
 console.log(ageJohn);
 console.log(ageMark);
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Lecture: if/else statements
+/*
+var name = 'John';
+var age = 26;
+var isMarried = 'no';
+
+// condition
+if (isMarried === 'yes') {
+  console.log(name + ' is married!');
+} else {
+  console.log(name + ' will + hopefully marry soon!');
+}
+
+isMarried = false;
+if (isMarried) {
+  console.log('YES');
+}else {
+  console.log('NO');
+}
+
+// == type coercion
+// === not type coercion
+if (23 == '23') {
+  console.log('Something to print...');
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Lecture: booelan logic and switch
+/*
+var age = 16;
+
+if (age < 20) {
+  console.log('John is a teenager');
+} else if (age > 20 && age < 30) {
+  console.log('John is a young man');
+} else {
+  console.log('John is a man');
+}
+
+var job = 'teacher';
+job = prompt('What does john do?');
+
+switch (job) {
+  case 'teacher':
+    console.log('John teaches kids');
+    break;
+    //break looping statements
+  case 'driver':
+    console.log('John drives a car in Lisbon');
+    break;
+  case 'cop':
+    console.log('John helps fight crime');
+    break;
+  default:
+    console.log('John does something else');
+}
+*/
