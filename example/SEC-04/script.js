4aaa75c07e445969f730be0bda448bef265b9727
lethisa <lethisaputri@gmail.com>
2018-02-06 12:44:39 +0700

    update #2

---
+++ example/SEC-04/script.js
@@ -0,0 +1,10 @@
+////////////////////////////////////////
+// lecture : booelan logic and switch //
+////////////////////////////////////////
+var age = 26;
+
+if (age <= 20) {
+  console.log("John is a teenager");
+} else {
+  console.log("John is a man");
+}
