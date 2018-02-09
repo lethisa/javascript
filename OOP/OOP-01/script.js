b047462029f707c3d0e382416f8f929ace7b8366
lethisa <lethisaputri@gmail.com>
2018-02-09 17:47:04 +0700

    add

--- OOP/OOP-01/script.js
+++ OOP/OOP-01/script.js
@@ -19,15 +19,20 @@ var Person = function(name, yearOfBirth, job) {
     };*/
 };
 ////////////////////// inheritance object
+// method inheritance
 Person.prototype.calculateAge =
   function() {
     console.log(2016 - this.yearOfBirth);
   };
+// variable inheritance
+Person.prototype.lastName = "Smith";
 
 ////////////////////// create john object - instance
 var jane = new Person("Jane", 1991, "police"); // new - new empty object created
 jane.calculateAge();
 var mike = new Person("Mike", 1993, "designer");
 mike.calculateAge();
-var mark = new Person("Markm 1967", "sailor");
+var mark = new Person("Mark", 1967, "sailor");
 mark.calculateAge();
+
+console.log(jane.lastName);
