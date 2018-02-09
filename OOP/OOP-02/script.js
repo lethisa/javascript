7c8d5464a5491c26e3b46435617487251e59182c
lethisa <lethisaputri@gmail.com>
2018-02-09 18:18:51 +0700

    add

---
+++ OOP/OOP-02/script.js
@@ -0,0 +1,27 @@
+/////////////////////////
+//OBJECT.CREATE METHOD //
+/////////////////////////
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "tacher";
+
+var jane = Object.create(personProto, {
+  name: {
+    value: "Jane"
+  },
+  yearOfBirth: {
+    value: 1968
+  },
+  job: {
+    value: "designer"
+  }
+});
+console.log(jane);
---
+++ OOP/OOP-04/script.js
@@ -0,0 +1,38 @@
+////////////////////////
+// PRIMITIVE X OBJECT //
+////////////////////////
+
+// primitive
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a); // 46
+console.log(b); // 23 => just copy varibale b, not reference anything
+
+// object
+var obj1 = {
+  name:"john",
+  age:26
+};
+
+var obj2 = obj1;
+obj1.age = 30; // => new reference point to obj1 - same object
+console.log(obj1.age); // 30
+console.log(obj2.age); // 30
+
+// function
+var age = 27;
+var obj = {
+  name: "lethisa",
+  city:"jakarta"
+};
+
+function change (a, b) {
+  a = 30; // not change anything
+  b.city = "US"; // point reference to object
+}
+// mutated variable
+change(age, obj);
+console.log(age);
+console.log(obj.city);
