83b07ae0794a834515fe72b21da534fa1ef7b6a3
lethisa <lethisaputri@gmail.com>
2018-02-13 13:18:30 +0700

    add

--- budget/app.js
+++ budget/app.js
@@ -1,6 +1,17 @@
 ////////////////////////////////////////////////////// BUDGET CONTROLLER
 
 var budgetController = (function() {
+  // function constructor
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
 })();
 
