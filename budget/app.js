0949b3099f42327072cbebc7dc1149345ef77e85
lethisa <lethisaputri@gmail.com>
2018-02-13 11:20:42 +0700

    add

--- budget/app.js
+++ budget/app.js
@@ -15,17 +15,20 @@ var UIController = (function() {
 ////////////////////////////////////////////////////// GLOBAL APP CONTROLLER
 
 var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
+
+  var ctrlAddItem = function() {
     // 1. get the field input DATA
     // 2. add the item to the budget controller
     // 3. add the item to the ui
     // 4. calculate the budgetCtrl
     // 5. display the budget on the ui
-  });
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
-      console.log("ENTER");
+      ctrlAddItem();
     }
   });
 
