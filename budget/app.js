69b53a7d62ca7a0f86e400a8e3bcd2c522217b98
lethisa <lethisaputri@gmail.com>
2018-02-13 12:28:30 +0700

    add

--- budget/app.js
+++ budget/app.js
@@ -14,7 +14,8 @@ var UIController = (function() {
   var DOMstring = {
     inputType: ".add__type",
     inputDescription: ".add__description",
-    inputValue: ".add__value"
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
   };
 
   return {
@@ -24,6 +25,9 @@ var UIController = (function() {
         description: document.querySelector(DOMstring.inputDescription).value,
         value: document.querySelector(DOMstring.inputValue).value
       };
+    },
+    getDOMstrings: function() {
+      return DOMstring;
     }
   };
 })();
@@ -32,17 +36,18 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function() {
     // 1. get the field input data
     var input = UICtrl.getInput();
-    
+
     // 2. add the item to the budget controller
     // 3. add the item to the ui
     // 4. calculate the budgetCtrl
     // 5. display the budget on the ui
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
