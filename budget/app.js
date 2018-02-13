cf3e95cceb86c6754c8f77a41e66b5fd21da1fcb
lethisa <lethisaputri@gmail.com>
2018-02-13 11:02:39 +0700

    add

--- budget/app.js
+++ budget/app.js
@@ -16,7 +16,11 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 document.querySelector(".add__btn").addEventListener("click", function() {
-
+    // 1. get the field input DATA
+    // 2. add the item to the budget controller
+    // 3. add the item to the ui
+    // 4. calculate the budgetCtrl
+    // 5. display the budget on the ui
 });
 
 })(budgetController, UIController);
