37858444cdeb10f50e2c7a7b328a25cb681f504e
lethisa <lethisaputri@gmail.com>
2018-03-24 17:11:10 +0700

    add

--- node/app.js
+++ node/app.js
@@ -8,20 +8,25 @@ const os = require('os');
 var user = os.userInfo();
 // load module from our project - note.js
 const notes = require('./notes.js');
-
+// calling function addNote from notes.js
+var res = notes.addNote();
+console.log(res);
+// calling function add from notes.js
+console.log('Result: ' + notes.add(9, -2));
 
 // original line
 /*fs.appendFile('greetings.txt','Hello World !');*/
 
 // option one - using text concatenate
-/*fs.appendFile('greetings.txt', 'Hello ' + user.username + '!' , function(err) {
+/*fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', function(err) {
   if (err) {
     console.log('Unable to write to file');
   }
-});
-*/
+});*/
+
 // option one - using ES6 template string
-fs.appendFile('greetings.txt', `Hello ${user.username}!` , function(err) {
+// call var age from notes.js
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}` , function(err) {
   if (err) {
     console.log('Unable to write to file');
   }
