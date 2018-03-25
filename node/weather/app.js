1cebeabb0bcf6092c35d897f2400849e86f3b1fd
lethisa <lethisaputri@gmail.com>
2018-03-25 21:10:06 +0700

    add

--- node/weather/app.js
+++ node/weather/app.js
@@ -27,13 +27,18 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&key=AIzaSyAuoiT1JCwMTCzE0SyWDs5uzsa1q0bk-eM`,
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  // get result from json
-
-  // get address
-  console.log(`address: ${body.results[0].formatted_address}`);
-  // get latitude
-  console.log(`latitude: ${body.results[0].geometry.location.lat}`);
-  //get longitude
-  console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+  // error exception
+  if (error) {
+    console.log('unable to connect to google server');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('unable to find that address');
+  } else if (body.status === 'OK') {
+    // console.log(JSON.stringify(body, undefined, 2)); => print all data
+    // get address
+    console.log(`address: ${body.results[0].formatted_address}`);
+    // get latitude
+    console.log(`latitude: ${body.results[0].geometry.location.lat}`);
+    //get longitude
+    console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
