7548f7a90775c545c1faaf8eb92349edc7ad2563
lethisa <lethisaputri@gmail.com>
2018-04-05 12:53:20 +0700

    add

--- node/node-api/server/server.js
+++ node/node-api/server/server.js
@@ -15,14 +15,17 @@ var {
   ObjectID
 } = require('mongodb');
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const _ = require('lodash');
 
 var app = express();
 app.listen(port, () => {
   console.log(`started on port ${port}`);
 });
+
+
 
 //////////////////////////////////////////////////////// POST
 
@@ -96,8 +99,45 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
 
+  }).catch((e) => {
+    res.status(400).send();
+    console.log(e);
+  });
+
+});
+
+//////////////////////////////////////////////////////// UPDATE
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send();
     console.log(e);
