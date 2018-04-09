593c91ac8c3542d11e496c416f53e8f602f83265
lethisa <lethisaputri@gmail.com>
2018-04-09 11:02:23 +0700

    add

--- node/node-api/server/server.js
+++ node/node-api/server/server.js
@@ -183,7 +183,22 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-//////////////////////////////////////////////////////// export
+//////////////////////////////////////////////////////// LOGIN
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    // res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+//////////////////////////////////////////////////////// EXPORT
 
 module.exports = {
   app
